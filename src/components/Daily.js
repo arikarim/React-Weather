d0e775909bd6ab2198eb07e7964a6357bc73489c
arikarim <arikarim146@gmail.com>
2021-05-28 01:33:00 +0300

    Add styling to hours

--- src/components/Daily.js
+++ src/components/Daily.js
@@ -6,11 +6,15 @@ const Daily = ({weather}) => {
     <div>
       {(typeof weather.location != "undefined") ? (
         <div className="my-5">
-          {weather.forecast.forecastday[0].hour.map((hour) => {
+          {weather.forecast.forecastday[0].hour.map((hour, index) => {
             return (
-              <div className="hours my-1 col-6 mx-auto d-flex">
-                <p className="mx-2">{hour.time.substring(10,20)}</p>
-                <p className="mx-2">{hour.condition.text}</p>
+              <div value={index} className="hours my-1 col-6 mx-auto d-flex justify-content-between">
+                <p className="mx-2 py-3">{hour.time.substring(10,20)}</p>
+                <p className="mx-2 py-3">{hour.condition.text}</p>
+                <div className="mx-2 py-3 d-flex justify-content-start">
+                  <p className="mx-2">{hour.temp_c}</p>
+                  <i class="wi wi-celsius"></i>
+                </div>
               </div>
             )
           })}
