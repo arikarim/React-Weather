6120171ea7be7b360fd19cd1b2282844d0ca3fe7
arikarim <arikarim146@gmail.com>
2021-05-27 22:01:45 +0300

    Add temperature

--- src/components/Child.js
+++ src/components/Child.js
@@ -5,10 +5,16 @@ const Child = ({weather}) => {
   return (
     <div className="">
       {(typeof weather.location != "undefined") ? (
-        <div className="d-flex justify-content-center">
-          <h2 className="mx-3">{weather.location.name}</h2>
-          <h2 className="mx-3">{weather.location.country}</h2>
+        <div>
+          <div className="d-flex justify-content-center">
+            <h2 className="mx-3">{weather.location.name}</h2>
+            <h2 className="mx-3">{weather.location.country}</h2>
+          </div>
+          <div className="my-3">
+            <p className="Temp">{weather.current.temp_c}</p>
+          </div>
         </div>
+        
         ) : ('')}
     </div>
   )
