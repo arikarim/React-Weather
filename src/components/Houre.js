80b682d3ebfcedec4452044a80c098ab7e452b4f
arikarim <arikarim146@gmail.com>
2021-05-29 01:02:59 +0300

    Mobile design for home page

--- src/components/Houre.js
+++ src/components/Houre.js
@@ -32,21 +32,21 @@ const Houre =({weather}) => {
             <i class="wi wi-night-clear"></i>
           </div>
 
-          <div className="my-2 d-flex col-12">
-            <div className="col-3 text-end justify-content-end align-self-center d-flex">
+          <div className="my-2 d-md-flex col-12">
+            <div className="col-3 mx-auto my-3  text-end justify-content-center justify-content-md-end align-self-center d-flex">
               Min <p className="my-0 mx-2">{weather.forecast.forecastday[0].day.mintemp_c}</p>
               <i class="wi wi-celsius"></i>
             </div>
 
-            <div className="my-6 temp-parent text-center col-4 mx-auto">
+            <div className="my-6 temp-parent text-center col-8 col-md-4 mx-auto">
               <div className="text-center fs-1">
                 <i class="wi wi-celsius"></i>
               </div>
               <p className="Temp">{weather.current.temp_c}</p>
             </div>
 
-            <div className="col-3 align-self-center d-flex">
-            Max <p className="my-0 mx-2">{weather.forecast.forecastday[0].day.maxtemp_c}</p>
+            <div className="col-3 mx-auto my-3 justify-content-center align-self-md-center d-flex">
+              Max <p className="my-0 mx-2">{weather.forecast.forecastday[0].day.maxtemp_c}</p>
             <i class="wi wi-celsius"></i>
             </div>
           </div>
