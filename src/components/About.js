04c829d6eacc2b85dc3a800f9167397fb80d94c0
arikarim <arikarim146@gmail.com>
2021-05-29 00:38:15 +0300

    Add icons

--- src/components/About.js
+++ src/components/About.js
@@ -9,7 +9,15 @@ const About = props => {
             you can search across the glob or find out your local weather just by typing your city name.</h6>
 
       </div>
-      <div className="about my-3 col-10 mx-auto d-flex justify-content-end">
+      <div className="about p-3 my-3 col-10 mx-auto d-flex justify-content-between">
+        <div className="d-flex justify-content-center col-4">
+          <ul className="list-unstyled icon-ul col-6">
+            <li className="icons "><a href="https://www.linkedin.com/in/ari-karim/"><i class="fab fa-linkedin"></i></a></li>
+            <li className="icons "><a href="https://github.com/arikarim"><i class="fab fa-github-square"></i></a></li>
+            <li className="icons "><a href="mailto:arikarim146@gmail.com"><i class="fas fa-envelope-square"></i></a></li>
+            <li className="icons "><a href="https://twitter.com/AriKari86036498"><i class="fab fa-twitter-square"></i></a></li>
+          </ul>
+        </div>
         <div className="col-8">
           <div className="fw-bolder">About the author:</div>
           <div className="fw-bolder">"Ari Karim"</div>
