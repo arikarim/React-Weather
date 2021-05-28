d50d63f24fcf24d02b8628d8742a6ef0cf35df7d
arikarim <arikarim146@gmail.com>
2021-05-28 22:40:23 +0300

    Add active class to navlinks

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 const Nav = () => (
   <div className="navbar d-flex py-3 justify-content-between">
     <h2 className="text-light">Weather</h2>
-    <ul>
-      <Link exact className="link-danger fw-bolder text-decoration-none mx-4" to="/">Home</Link>
+    <ul className="navbar-ul">
+      <NavLink activeClassName="active" exact className="navbar-li  text-decoration-none mx-4" to="/">Home</NavLink>
 
-      <Link exact className="link-danger fw-bolder text-decoration-none mx-4" to="/hour">24 Hour</Link>
+      <NavLink activeClassName="active" exact className="navbar-li  text-decoration-none mx-4" to="/hour">24 Hour</NavLink>
     </ul>
   </div>
 );
