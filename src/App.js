afb0cb27754ae510027280ab99385fa8068bad58
arikarim <arikarim146@gmail.com>
2021-05-29 21:59:15 +0300

    Remove commented code

--- src/App.js
+++ src/App.js
@@ -1,12 +1,1 @@
 // import './App.css';
-// import Api from './components/Api'
-
-// function App() {
-//   return (
-//     <div>
-//       <Api />
-//     </div>
-//   );
-// }
-
-// export default App;
